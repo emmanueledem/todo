7c8a6eedccf45f64a3aaf022ece7a9de4758a29a
emmanuel <emmanuelokon591@gmail.com>
2022-02-24 15:16:28 +0000

    CreatedApp

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:todo/screens/task_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Task_Screen(),
+    );
+  }
+}
---
+++ lib/screens/task_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Task_Screen extends StatefulWidget {
+  const Task_Screen({Key? key}) : super(key: key);
+
+  @override
+  _Task_ScreenState createState() => _Task_ScreenState();
+}
+
+class _Task_ScreenState extends State<Task_Screen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
